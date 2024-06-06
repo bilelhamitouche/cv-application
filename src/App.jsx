162ad99ca4a81a5bd6acfcb634048e38b6b55c51
bilelhamitouche <bilelhamitouche4@gmail.com>
2024-06-06 19:55:29 +0100

    making some changes to the overall structure of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import './App.css';
+import { GeneralInfoForm } from './components/form/GeneralInfoForm';
+import { EducationForm } from './components/form/EducationForm';
+import { SkillsForm } from './components/form/SkillsForm';
+import { ExperienceForm } from './components/form/ExperienceForm';
+import { GeneralInfoPreview } from './components/preview/GeneralInfoPreview';
+import { EducationPreview } from './components/preview/EducationPreview';
 
 function App() {
-  return <></>;
+  function togglerFunction(e) {
+    e.target.parentNode.childNodes[1].classList.toggle('visible');
+  }
+
+  return (
+    <div className="App">
+      <div className="cv-form">
+        <GeneralInfoForm clickHandler={togglerFunction} />
+        <ul className="education-list">
+          <EducationForm clickHandler={togglerFunction} />
+          <EducationForm clickHandler={togglerFunction} />
+          <EducationForm clickHandler={togglerFunction} />
+        </ul>
+        <ul className="experience-list">
+          <ExperienceForm clickHandler={togglerFunction} />
+          <ExperienceForm clickHandler={togglerFunction} />
+          <ExperienceForm clickHandler={togglerFunction} />
+        </ul>
+        <SkillsForm clickHandler={togglerFunction} />
+      </div>
+      <div className="cv-preview">
+        <GeneralInfoPreview />
+        <EducationPreview />
+      </div>
+    </div>
+  );
 }
 
 export default App;
