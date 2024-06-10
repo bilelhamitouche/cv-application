a2d8ff4fc699287eeee77eb7f9a9bad1979dedca
bilelhamitouche <bilelhamitouche4@gmail.com>
2024-06-10 02:58:40 +0100

    added dummy data

---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+
+const initialGeneralInfo = {
+  fullName: 'Bilel Hamitouche',
+  designation: 'Web developer',
+  address: 'Cité evolutif Bt 12 N05 Bab El Oued',
+  email: 'example@gmail.com',
+  phone: 585566333,
+};
+
+export { initialGeneralInfo };
